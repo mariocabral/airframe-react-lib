318851baece89bbdd941768ab30a1487df9c8ec2
mario <mlc-lab@protonmail.com>
2020-10-09 18:13:27 -0300

    add components definitins from https://github.com/0wczar/airframe-react project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,157 @@
-import React from 'react'
-import styles from './styles.module.css'
-
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+import Accordion from './components/Accordion';
+import Avatar, { AvatarAddOn } from './components/Avatar';
+import Card from './components/Card';
+import CardHeader from './components/CardHeader';
+import Checkable from './components/Checkable';
+import CustomInput from './components/CustomInput';
+import Divider from './components/Divider';
+import EmptyLayout from './components/EmptyLayout';
+import ExtendedDropdown from './components/ExtendedDropdown';
+import FloatGrid from './components/FloatGrid';
+import HolderProvider from './components/HolderProvider';
+import IconWithBadge from './components/IconWithBadge';
+import InputGroupAddon from './components/InputGroupAddon';
+import Layout, {
+    withPageConfig,
+    setupPage,
+    PageConfigProvider,
+    PageConfigConsumer
+} from './components/Layout';
+import Nav from './components/Nav';
+import Navbar from './components/Navbar';
+import NavbarThemeProvider from './components/NavbarThemeProvider';
+import NestedDropdown from './components/NestedDropdown';
+import OuterClick from './components/OuterClick';
+import PageLoader from './components/PageLoader';
+import Progress from './components/Progress';
+import Sidebar from './components/Sidebar';
+import SidebarMenu from './components/SidebarMenu';
+import SidebarTrigger from './components/SidebarTrigger';
+import StarRating from './components/StarRating';
+import {
+    ThemeClass,
+    ThemeProvider,
+    ThemeSelector,
+    ThemeConsumer,
+} from './components/Theme';
+import Tools from './components/Tools';
+import UncontrolledModal from './components/UncontrolledModal';
+import UncontrolledPopover from './components/UncontrolledPopover';
+import UncontrolledTabs from './components/UncontrolledTabs';
+import Wizard from './components/Wizard';
+// Export non overriden Reactstrap components
+export {
+    Alert,
+    Badge,
+    Breadcrumb,
+    BreadcrumbItem,
+    Button,
+    ButtonDropdown,
+    ButtonGroup,
+    ButtonToolbar,
+    CardBody,
+    CardColumns,
+    CardDeck,
+    CardFooter,
+    CardGroup,
+    CardImg,
+    CardImgOverlay,
+    CardLink,
+    CardSubtitle,
+    CardText,
+    CardTitle,
+    Carousel,
+    CarouselCaption,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselItem,
+    Col,
+    Collapse,
+    Container,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle,
+    Fade,
+    Form,
+    FormFeedback,
+    FormGroup,
+    FormText,
+    Input,
+    InputGroup,
+    InputGroupButtonDropdown,
+    InputGroupText,
+    Jumbotron,
+    Label,
+    ListGroup,
+    ListGroupItem,
+    ListGroupItemHeading,
+    ListGroupItemText,
+    Media,
+    Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+    NavbarBrand,
+    NavbarToggler,
+    NavItem,
+    NavLink,
+    Pagination,
+    PaginationItem,
+    PaginationLink,
+    Popover,
+    PopoverBody,
+    PopoverHeader,
+    Row,
+    TabContent,
+    Table,
+    TabPane,
+    Tooltip,
+    UncontrolledAlert,
+    UncontrolledButtonDropdown,
+    UncontrolledDropdown,
+    UncontrolledCollapse,
+    UncontrolledTooltip
+} from 'reactstrap';
+export {
+    Accordion,
+    //App,
+    Avatar,
+    AvatarAddOn,
+    Card,
+    CardHeader,
+    Checkable,
+    CustomInput,
+    Divider,
+    EmptyLayout,
+    ExtendedDropdown,
+    FloatGrid,
+    IconWithBadge,
+    InputGroupAddon,
+    HolderProvider,
+    Layout,
+    Nav,
+    Navbar,
+    NavbarThemeProvider,
+    NestedDropdown,
+    withPageConfig,
+    setupPage,
+    OuterClick,
+    PageLoader,
+    PageConfigConsumer,
+    PageConfigProvider,
+    Progress,
+    Sidebar,
+    SidebarMenu,
+    SidebarTrigger,
+    StarRating,
+    ThemeClass,
+    ThemeConsumer,
+    ThemeProvider,
+    ThemeSelector,
+    Tools,
+    UncontrolledPopover,
+    UncontrolledTabs,
+    UncontrolledModal,
+    Wizard
+};
